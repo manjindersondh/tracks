0d42260949b32178d70a24e1261b052f409bd5a4
manjindersondh <63088923+manjindersondh@users.noreply.github.com>
2020-04-03 11:17:53 +0530

    track
    track

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://a1151ba5.ngrok.io'
+});
---
+++ src/screens/TrackCreateScreen.js
@@ -0,0 +1,44 @@
+import '../_mockLocation';
+import React, { useEffect, useState } from 'react';
+import {StyleSheet} from 'react-native';
+import {Text} from 'react-native-elements';
+import {SafeAreaView} from 'react-navigation';
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
+import Map from '../components/Map';
+
+
+const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null);
+    const starWatching = async () =>{
+        try {
+            await requestPermissionsAsync();
+            await watchPositionAsync({
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 1000,
+                distanceInterval: 10
+            }, location => {
+                console.log(location);
+            });
+
+        } catch (e) {
+            setErr(e);
+
+        }
+    };
+
+    useEffect(()=> {
+        starWatching();
+    }, []);
+
+    return(
+        <SafeAreaView forceInset={{top: 'always'}} >
+            <Text h2> Create a Track </Text>
+            <Map/>
+            {err ? <Text>Please enable location services</Text> : null}
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackCreateScreen;
